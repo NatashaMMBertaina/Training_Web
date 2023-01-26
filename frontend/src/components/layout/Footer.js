ea4a8d02e98d55dfcba5dbd16ea88a6f2b21dfa3
NatashaMMBertaina <natubertaina@gmail.com>
2023-01-26 10:20:03 -0300

    back

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -8,9 +8,9 @@ const Footer = (props) => {
             <section className="holder">
                 <div className="contact">
                     <p className="fs-5 text-decoration-underline">Contacto:</p>
-                    <p>instituto.bioingenieria@um.edu.ar</p>
-                    <p>750 Paseo Doctor Emilio Descotte, Mendoza M5500 </p>
+                    <p>sonounoteam@gmail.com</p>
                     <p>Facultad de Ingenieria, Universidad de Mendoza</p>
+                    <p>Instituto de Tecnologías en Detección y Astropartículas</p>
                 </div>
                 <div className="logof">
                     <img src="images/logofooter.png" width={80} alt="Logos Universidad de Mendoza y Reinforce"/>
